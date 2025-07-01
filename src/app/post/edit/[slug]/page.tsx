4f0a113ef8ebed59007228c56bc4705ecdb22ea8
eboateng <eboat086@gmail.com>
2025-07-01 12:45:44 +0000

    implemented header and first part of auth page using shadcn

---
+++ src/app/post/edit/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default function EditPostPage() {
+  return (
+    <div>
+      <h1>Edit Post Page</h1>
+    </div>
+  );
+}
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,53 @@
 "use client";
 
-function Header() {
-  return <div>Header</div>;
-}
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
-export default Header;
+export default function Header() {
+  const router = useRouter();
+
+  const navItems = [
+    { label: "Home", href: "/" },
+    { label: "Create", href: "/post/create" },
+  ];
+
+  return (
+    <header className="border-b bg-background sticky top-0 z-10">
+      <div className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <div className="flex items-center gap-6">
+          <Link className="font-bold text-xl" href="/">
+            Next JS 15 Blog
+          </Link>
+
+          <nav className="hidden md:flex items-center gap-6">
+            {navItems.map((navItem) => (
+              <Link
+                key={navItem.href}
+                href={navItem.href}
+                className={cn(
+                  "text-sm font-medium transition-colors hover:text-primary"
+                )}
+              >
+                {navItem.label}
+              </Link>
+            ))}
+          </nav>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="hidden md:block">{/* Placeholder for Search */}</div>
+          {/* Placeholder for theme toggle */}
+          <div className="flex items-center gap-2">
+            <Button
+              className="cursor-pointer"
+              onClick={() => router.push("/auth")}
+            >
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
